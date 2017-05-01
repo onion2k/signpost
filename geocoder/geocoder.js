1aa9d467d279f38a8fffc2042a4f97e67cc1f3cd
Chris <chris@usablehq.com>
2017-05-01 17:09:14 +0100

    Added geolib to the geocoder

--- geocoder/geocoder.js
+++ geocoder/geocoder.js
@@ -1,5 +1,6 @@
 var env = require('../.env');
 var NodeGeocoder = require('node-geocoder');
+var geolib = require('geolib');
 
 var options = {
   provider: 'google',
@@ -10,13 +11,44 @@ var options = {
   formatter: null         // 'gpx', 'string', ...
 };
 
-var geocoder = NodeGeocoder(options);
+// var geocoder = NodeGeocoder(options);
 
-// Or using Promise
-geocoder.geocode('29 champs elysée paris')
-  .then(function(res) {
-    console.log(res);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+// // Or using Promise
+// geocoder.geocode('New York, USA')
+//   .then(function(res) {
+//     console.log(res);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
+var locations = {
+  "sunderland": {
+    "latitude": 54.9145215,
+    "longitude": -1.3709684
+  },
+  "london": {
+    "latitude": 51.5073509,
+    "longitude": -0.1277583
+  },
+  "newcastle": {
+    "latitude": 54.978252,
+    "longitude": -1.61778
+  },
+  "newyork": {
+    "latitude": 40.7127837,
+    "longitude": -74.0059413,
+  }
+}
+
+var calcDist = function(from, to) {
+  console.log("From "+from+" to "+to);
+  console.log("distance: "+geolib.convertUnit('mi', geolib.getDistance(locations[from], locations[to])));
+  console.log("bearing: "+geolib.getBearing(locations[from], locations[to]));
+  console.log("direction: "+geolib.getCompassDirection(locations[from], locations[to]).rough+" "+geolib.getCompassDirection(locations[from], locations[to]).exact);
+  console.log("");
+}
+
+calcDist('sunderland', 'newcastle');
+calcDist('sunderland', 'london');
+calcDist('sunderland', 'newyork');
