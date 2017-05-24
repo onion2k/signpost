bcb0a49d567d745ccaaa43d750aa332c266724c2
Chris <chris@usablehq.com>
2017-05-24 20:44:06 +0100

    API Update for load, save, print

--- api/app.js
+++ api/app.js
@@ -28,9 +28,19 @@ io.on('connection', function (socket) {
       } else {
         result[0].index = -1;
       }
-      console.log(result);
       socket.emit('geocode', result);
     });
+  });
+  socket.on('save', function(data){
+      socket.emit('save', { saved: true });
+  });
+  socket.on('load', function(){
+      socket.emit('print', {
+        sign: [{ place: 'France', index: 0, id: 'france', active: false }, { place: 'London', index: 1, id: 'london', active: false }, { place: 'Germany', index: 2, id: 'germany', active: false }, { place: 'Sunderland, UK', index: 3, id: 'sunderland', active: false }]
+      });
+  });
+  socket.on('print', function(data){
+      socket.emit('print', { printing: 'PC Load Letter' });
   });
 });
 
