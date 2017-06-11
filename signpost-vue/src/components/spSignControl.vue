494adaad0aa6ae40531c4ef700c2a6a99b062eb8
Chris <chris@usablehq.com>
2017-06-11 10:44:19 +0100

    Signpost rotation controls added

---
+++ signpost-vue/src/components/spSignControl.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="controls">
+        <button type="button" v-on:click="reverse()" class="btn orange waves-effect waves-light">Reverse</button>
+        <button type="button" v-on:click="stop()" class="btn red waves-effect waves-light">Stop</button>
+        <button type="button" v-on:click="play()" class="btn green waves-effect waves-light">Play</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'spSignControl',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    reverse () {
+        signpostRotator.reverse();
+    },
+    stop () {
+        signpostRotator.pause();
+    },
+    play () {
+        signpostRotator.play();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #controls {
+        position: absolute;
+        right: 0;
+        padding: 10px;
+    }
+</style>
--- signpost-vue/src/components/spThreeViewer.vue
+++ signpost-vue/src/components/spThreeViewer.vue
@@ -1,10 +1,19 @@
 <template>
+  <div>
+    <spSignControl></spSignControl>
     <div style="min-height: 600px; padding: 0;" id="wrapper"></div>
+  </div>
 </template>
 
 <script>
+
+import spSignControl from '@/components/spSignControl.vue';
+
 export default {
   name: 'spThreeViewer',
+  components: {
+    spSignControl
+  },
   data () {
     return {
 
